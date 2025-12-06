6aa95d582530f0c66e5389e08d95ae2047856749
Prin0u <geoffreychenivess@hotmail.fr>
2025-12-06 20:04:04 +0100

    Système d'authentification fonctionnel

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Prin0u\DevoirAppMvcPhp\Controllers;
+
+use Prin0u\DevoirAppMvcPhp\Core\Controller;
+use Prin0u\DevoirAppMvcPhp\Core\Database;
+use Prin0u\DevoirAppMvcPhp\Core\Session;
+
+
+
+class AuthController extends Controller
+{
+    // Affichage du formulaire
+    public function login()
+    {
+        $this->render('auth/login');
+    }
+    
+    // Traitement du formulaire
+    public function loginPost()
+    {   
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+    
+        try {
+            $pdo = Database::getInstance();
+    
+            $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE email = ?");
+            $stmt->execute([$email]);
+            $user = $stmt->fetch(\PDO::FETCH_ASSOC);
+    
+            if (!$user || !password_verify($password, $user['password'])) {
+                $_SESSION['flash_error'] = "Email ou mot de passe incorrect.";
+                header('Location: /login');
+                exit;
+            }
+    
+            // Connexion réussie
+            $_SESSION['user'] = [
+                'id' => $user['id_user'],
+                'nom' => $user['nom'],
+                'prenom' => $user['prenom'],
+                'email' => $user['email']
+            ];
+    
+            header('Location: /');
+            exit;
+    
+        } catch (\PDOException $e) {
+            // Affiche l’erreur PDO pour debug
+            echo "Erreur PDO : " . $e->getMessage();
+            exit;
+        }
+    }
+    
+
+    // Deconnexion
+    public function logout()
+    {
+        session_destroy();
+        header('Location: /login');
+        exit;
+    }
+}
