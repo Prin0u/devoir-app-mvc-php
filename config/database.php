c4660a3ffa886cfa495fe3f9d73af76970d983c2
Prin0u <geoffreychenivess@hotmail.fr>
2026-04-22 18:54:40 +0200

    fix env var

--- config/database.php
+++ config/database.php
@@ -3,6 +3,6 @@ return [
     'host'     => getenv('DB_HOST'),
     'dbname'   => getenv('DB_NAME'),
     'user'     => getenv('DB_USER'),
-    'password' => getenv('DB_PASS'),
+    'password' => getenv('DB_PASSWORD'),
     'port'     => getenv('DB_PORT') ?: '5432',
 ];
